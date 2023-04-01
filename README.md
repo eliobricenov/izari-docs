fbb2642a67ae584b8f44699088245c1c924ba45b
Elio Briceño <eliobricenov@gmail.com>
2023-03-31 20:20:38 -0400

    feat: add docs pages

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
-# Nextra Docs Template 
-
-This is a template for creating documentation with [Nextra](https://nextra.site).
+# Izari Docs
 
 [**Live Demo →**](https://nextra-docs-template.vercel.app)
 
 [![](.github/screenshot.png)](https://nextra-docs-template.vercel.app)
 
-## Quick Start
+## Tools
 
-Click the button to clone this repository and deploy it on Vercel:
+- [Next.js](https://nextjs.org)
+- [Nextra](https://nextra.site/)
+- [Tailwind CSS](https://tailwindcss.com/)
 
-[![](https://vercel.com/button)](https://vercel.com/new/clone?s=https%3A%2F%2Fgithub.com%2Fshuding%2Fnextra-docs-template&showOptionalTeamCreation=false)
-
-## Local Development
-
-First, run `pnpm i` to install the dependencies.
-
-Then, run `pnpm dev` to start the development server and visit localhost:3000.
-
-## License
-
-This project is licensed under the MIT License.
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import ZondaxLogo from "./components/ZondaxLogo";
+import Footer from "./components/Footer";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  head: (
+    <>
+      <title>Izari Docs</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Izari Docs" />
+      <meta property="og:description" content="Izari Doc" />
+    </>
+  ),
+  logo: ZondaxLogo,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/zondax/izari-filecoin",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  gitTimestamp: <></>,
+  docsRepositoryBase: "https://github.com/zondax/izari-filecoin",
   footer: {
-    text: 'Nextra Docs Template',
+    text: Footer,
   },
-}
+};
 
-export default config
+export default config;
